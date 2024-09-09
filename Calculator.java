ef385a9513fc9d0683894627f2dc78efa0ccf716
ian3010 <jmnyx123@gmail.com>
2024-09-10 02:37:53 +0800

    final

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,6 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Simple Calculator");
     }
-}
 
 public static int add(int a, int b) { return a + b; }
 public static int subtract(int a, int b) { return a-b; }
