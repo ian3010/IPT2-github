c2d1eb255e3edfa1cd38cc871f5efbb4ec3f6b47
ian3010 <jmnyx123@gmail.com>
2024-09-10 02:35:10 +0800

    Add division method

--- Calculator.java
+++ Calculator.java
@@ -8,3 +8,4 @@ public static int add(int a, int b) { return a + b; }
 public static int subtract(int a, int b) { return a-b; }
 
 public static int multiply(int a, int b) { return a*b;}
+public static int divide(int a, int b) { return a/b;}
