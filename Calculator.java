4fab7ec2419bf4ad8f37a4cad827e23cf35fbd51
ian3010 <jmnyx123@gmail.com>
2024-09-10 03:01:01 +0800

    Final Simple calculator

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,43 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Simple Calculator");
+
+        int a = 8;
+        int b = 2;
+
+        int addResult = add(a, b);
+        System.out.println("Addition: " + addResult);
+
+        int subtractResult = subtract(a, b);
+        System.out.println("Subtraction: " + subtractResult);
+
+        int multiplyResult = multiply(a, b);
+        System.out.println("Multiplication: " + multiplyResult);
+
+        int divideResult = divide(a, b);
+        System.out.println("Division: " + divideResult);
+
+        int modulusResult = modulus(a, b);
+        System.out.println("Modulus: " + modulusResult);
     }
 
-public static int add(int a, int b) { return a + b; }
-public static int subtract(int a, int b) { return a-b; }
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
-public static int multiply(int a, int b) { return a*b;}
-public static int divide(int a, int b) { return a/b;}
-public static int modulus(int a, int b) { return a%b;} }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+}
