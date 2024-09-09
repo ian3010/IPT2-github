33ad504064d163b73e9f571f4ee122f8d412cb57
ian3010 <jmnyx123@gmail.com>
2024-09-10 02:36:17 +0800

    Add modulus method

--- Calculator.java
+++ Calculator.java
@@ -9,3 +9,4 @@ public static int subtract(int a, int b) { return a-b; }
 
 public static int multiply(int a, int b) { return a*b;}
 public static int divide(int a, int b) { return a/b;}
+public static int modulus(int a, int b) { return a%b;} }
